9e96837d899374f4af96cb136a1baeb496bb29fc
Arnel Almazan <almazan.rnl@gmail.com>
2020-06-09 00:18:03 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from 'antd';
+import about from './assets/images/about.png'
+import portfolio from './assets/images/portfolio.svg'
+import exemplar from './assets/images/exemplar.svg'
+import './App.scss';
+
+const { Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Layout>
+          <Content>
+            <div>
+              <p>Jessmer Nicole</p>
+            </div>
+            <div className="text-links">
+              <span>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  ABOUT
+                </a>
+                &nbsp;&nbsp;|&nbsp;&nbsp;
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  BLOG
+                </a>
+                &nbsp;&nbsp;|&nbsp;&nbsp;
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  PORTFOLIO
+                </a>
+                &nbsp;&nbsp;|&nbsp;&nbsp;
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  EXEMPLAR
+                </a>
+                &nbsp;&nbsp;|&nbsp;&nbsp;
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  CONTACT
+                </a>
+              </span>
+            </div>
+            <div className="image-links">
+              <img src={about} alt="about-me-link"/>
+              <img src={portfolio} alt="portfolio-link" style={{ width: 80 }} />
+              <img src={exemplar} alt="exemplar-link" style={{ width: 80 }} />
+            </div>
+          </Content>
+        </Layout>
     </div>
   );
 }

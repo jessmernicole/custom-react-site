434e79430c4e645e724bff532cb2fa9aca481a7c
Arnel Almazan <almazan.rnl@gmail.com>
2020-07-02 23:39:44 +0800

    yarn build

---
+++ build/precache-manifest.d213148d4f4918f0d4f756320b7d6996.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd0e5b5adcd2e72152512810aec4a44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfc969dfdd7cf8c9a412",
+    "url": "/static/css/main.0cea1cac.chunk.css"
+  },
+  {
+    "revision": "9d4b9da361a31d67a6e5",
+    "url": "/static/js/2.7b1de794.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/static/js/2.7b1de794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc969dfdd7cf8c9a412",
+    "url": "/static/js/main.4c221cf5.chunk.js"
+  },
+  {
+    "revision": "75cfcc6f31f143a38e0f",
+    "url": "/static/js/runtime-main.0abb84bb.js"
+  },
+  {
+    "revision": "dfbef270ec14d13ef4e49781f9c245a9",
+    "url": "/static/media/Amsterdam.dfbef270.ttf"
+  },
+  {
+    "revision": "739d98b1cf532a8f157e69448db57e76",
+    "url": "/static/media/home-about.739d98b1.jpg"
+  },
+  {
+    "revision": "2725a915e22ab78155f64ab277a69244",
+    "url": "/static/media/instagram.2725a915.svg"
+  },
+  {
+    "revision": "169cd688b82132030581dc1c377b9267",
+    "url": "/static/media/linkedin.169cd688.svg"
+  }
+]);
